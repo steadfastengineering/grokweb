c609a5431d9bd1a70828c345235ab3d0f8d4cfbc
steadfastengineering <steadfastengineering@protonmail.com>
2025-03-23 21:57:36 -0700

    Action bar is shown for new theme, settings button works, apk size is still way too large.

--- app/src/main/java/com/steadfast/webgrok/MainActivity.kt
+++ app/src/main/java/com/steadfast/webgrok/MainActivity.kt
@@ -1,35 +1,38 @@
 
 package com.steadfast.webgrok
 
-import android.graphics.Color
+import android.graphics.Color.BLACK
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import android.view.Menu
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import org.mozilla.geckoview.GeckoRuntime
 import org.mozilla.geckoview.GeckoRuntimeSettings.COLOR_SCHEME_DARK
 import org.mozilla.geckoview.GeckoSession
 import org.mozilla.geckoview.GeckoView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
 
+    lateinit var context:MainActivity
     private lateinit var geckoView: GeckoView
     private lateinit var geckoSession: GeckoSession
     private lateinit var geckoRuntime: GeckoRuntime
     private var viewSessionState: GeckoSession.SessionState? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        context = this
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
         // Configure the view settings
         geckoView = findViewById(R.id.geckoview)
-        geckoView.setBackgroundColor(Color.BLACK)
+        geckoView.setBackgroundColor(BLACK)
         geckoView.autofillEnabled = true
-        
+
         // Configure the session and runtime
         geckoSession = GeckoSession()
-        geckoRuntime = GeckoRuntime.create(this)
+        geckoRuntime = GeckoRuntime.create(context)
         geckoRuntime.settings.setPreferredColorScheme(COLOR_SCHEME_DARK)
         geckoSession.open(geckoRuntime)
         geckoView.setSession(geckoSession)
@@ -44,59 +47,28 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
     private fun createProgressDelegate(): GeckoSession.ProgressDelegate {
         return object : GeckoSession.ProgressDelegate {
-
             override fun onSessionStateChange( session: GeckoSession, sessionState: GeckoSession.SessionState) {
                 super.onSessionStateChange(session, sessionState)
-                // Anytime the session state changes, save it here.
+                // Anytime the Geckoview session state changes, save it here to our activity member.
                 viewSessionState = sessionState
             }
         }
     }
-
-  override fun onSaveInstanceState(outState: Bundle) {
-    super.onSaveInstanceState(outState)
-    //sessionState = geckoSession.sessionState
-    //outState.putParcelable("sessionState", sessionState)
-}
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        //sessionState = savedInstanceState.getParcelable("sessionState")
-    }
 }
 
 
-
-
-//package com.steadfast.webgrok
-//
-//import android.os.Bundle
-//import androidx.activity.ComponentActivity
-//import androidx.appcompat.app.AppCompatActivity
-//import org.mozilla.geckoview.GeckoRuntime
-//import org.mozilla.geckoview.GeckoSession
-//import org.mozilla.geckoview.GeckoView
-//
-//class MainActivity : ComponentActivity() {
-//
-//    private lateinit var geckoView: GeckoView
-//    private lateinit var geckoSession: GeckoSession
-//    private lateinit var geckoRuntime: GeckoRuntime
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//
-//        setContentView(R.layout.activity_main)
-//
-//        geckoView = findViewById(R.id.geckoview)
-//        geckoSession = GeckoSession()
-//        geckoRuntime = GeckoRuntime.create(this)
-//
-//        geckoSession.open(geckoRuntime)
-//        geckoView.setSession(geckoSession)
-//        geckoSession.loadUri("https://google.com")
-//
-//    }
-//}
